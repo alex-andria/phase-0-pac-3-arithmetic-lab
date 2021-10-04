d47098b2826cd99fc3c93cefacc9784c20e5895f
Alexandria DAntonio <adantonio@wayne.edu>
2021-10-04 02:04:49 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(num1, num2){
+    let total = num1 + num2;
+    return total;
+}
+
+function subtract(num1, num2){
+    let total = num1 - num2;
+    return total;
+}
+
+function multiply(num1, num2){
+    let total = num1 * num2;
+    return total;
+}
+
+function divide(num1, num2){
+    let total = num1 / num2;
+    return total;
+}
+
+function increment(n){
+    let total = n+=1;
+    return total;
+}
+
+function decrement(n){
+    let total = n-=1;
+    return total;
+}
+
+function makeInt(n){
+    let total = parseInt(n,10);
+    return total;
+}
+
+function preserveDecimal(n){
+    let total = parseFloat(n);
+    return total;
+}
